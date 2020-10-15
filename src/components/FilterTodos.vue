184460eaf00f1c9af2bf9b19914bf431a5437f5a
fring-12 <banedictfring12@gmail.com>
2020-10-15 11:03:53 +0600

    push all

---
+++ src/components/FilterTodos.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="filter">
+    <h3>Filter Todos</h3>
+    <select @change="filterTodos($event)">
+      <option value="200">200</option>
+      <option value="100">100</option>
+      <option value="50">50</option>
+      <option value="20">20</option>
+      <option value="5">5</option>
+    </select>
+
+  </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+
+  export default {
+    name: "FilterTodos",
+    methods: mapActions(["filterTodos"])
+  }
+</script>
+
+<style scoped>
+.filter{
+  display: flex;
+}
+select{
+  padding: 5px;
+  margin: 20px;
+  border: 1px solid #41b883;
+  border-radius: 2px;
+}
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+import todos from './modules/todos'
+
+//load vuex
+
+Vue.use(Vuex);
+
+//create store 
+
+export default new Vuex.Store({
+  modules: {
+    todos
+  }
+});
